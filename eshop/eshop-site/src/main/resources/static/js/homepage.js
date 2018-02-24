15dfeaf500f40ed4c14b8828516bd175478413a4
Raji Reddy <rajireddy.raj@gmail.com>
2018-02-24 22:51:34 +0530

    commit x
    user,role,permission added

--- eshop/eshop-site/src/main/resources/static/js/homepage.js
+++ eshop/eshop-site/src/main/resources/static/js/homepage.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
 	}).then(function(data){
 		console.log(data);
 		for(let value of data){
+			var imgUrl = "";
+			if(value.name === "Speakers"){
+				imgUrl = "https://goo.gl/xy7K3y";
+			}
+			else if(value.name === "Televisions"){
+				imgUrl = "https://goo.gl/JoNmWE";
+			}
 			$("#categories").append(
 					'<div class="container-fluid bg-white marginbtm3">\
 						<h3 class="text-center"><a href="#">View All '+value.name+'</a></h3>\
@@ -18,7 +25,7 @@ $(document).ready(function() {
 				$("#"+value.name).append(
 						'<div class="col-lg-3 col-md-4 col-xs-6">\
 				          <a href="#" class="d-block mb-4 h-100">\
-				            <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">\
+				            <img class="img-fluid img-thumbnail" src="'+imgUrl+'" alt="">\
 				          	<p>'+value.products[i].name+'</p>\
 				          	<p>Rs. '+value.products[i].price+'</p>\
 				          </a>\
