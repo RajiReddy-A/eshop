64f3d139088e737aaa840f8c046225eb71c05434
Raji Reddy <rajireddy.raj@gmail.com>
2018-03-04 22:42:35 +0530

    commit x
    owl carousel

--- eshop/eshop-site/src/main/resources/static/js/main.js
+++ eshop/eshop-site/src/main/resources/static/js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($){
+$(window).load(function(){
     
     // jQuery sticky Menu
     
--- eshop/eshop-site/src/main/resources/static/js/productdetails.js
+++ eshop/eshop-site/src/main/resources/static/js/productdetails.js
@@ -33,15 +33,16 @@ $(document).ready(function(){
 					<ins>Rs. '+data.price+'</ins>\
 					');
 			$("#categoryLink").append(data.category.name);
+			$("#descriptionText").append(data.description);
 					
 			theCategory = data.category.name;
 			var relatedUrl = window.location.protocol + "//" + window.location.host +"/api/category/"+theCategory;
 			$.ajax({
 				url: relatedUrl
-			}).then(function(data){
+			}).then(function(data2){
 				
 				var i=1;
-				for(let value of data.products){
+				for(let value of data2.products){
 					if(i===6){
 						break;
 					}
@@ -50,12 +51,68 @@ $(document).ready(function(){
 					$("#sidebarProducts").append('\
 							<div class="thubmnail-recent">\
 				                <img src="'+value.imageUrl+'" class="recent-thumb" alt="">\
-				                <h2><a href="">'+value.name+'</a></h2>\
+				                <h2><a href="/products?product='+value.name+'">'+value.name+'</a></h2>\
 				                <div class="product-sidebar-price">\
 				                    <ins>Rs. '+value.price+'</ins>\
 				                </div>\
 							</div>\
 					');
+				}
+				
+			});
+			
+			var allCategoriesUrl = window.location.protocol + "//" + window.location.host +"/api/allcategories/";
+			$.ajax({
+				url: allCategoriesUrl
+			}).then(function(data3){
+				
+				for(let value of data3){
+					if(value.name !== data.category.name){
+						for(let eachProduct of value.products){
+							$("#otherProducts").append('\
+									<div class="single-product">\
+	                                    <div class="product-f-image">\
+	                                        <img src="'+eachProduct.imageUrl+'" alt="">\
+	                                        <div class="product-hover">\
+	                                            <a href="" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>\
+	                                            <a href="/products?product='+eachProduct.name+'" class="view-details-link"><i class="fa fa-link"></i> See details</a>\
+	                                        </div>\
+	                                    </div>\
+	                                    <h2><a href="/products?product='+eachProduct.name+'">'+eachProduct.name+'</a></h2>\
+	                                    \
+	                                    <div class="product-carousel-price">\
+	                                        <ins>Rs. '+eachProduct.price+'</ins>\
+	                                    </div>\
+	                                </div>\
+									');
+						}
+					}
+					
+					/*$('.related-products-carousel').data('owlCarousel').destroy();
+					//$('.related-products-carousel').owlCarousel('update');
+					
+					$('.related-products-carousel').owlCarousel({
+				        loop:true,
+				        nav:true,
+				        margin:20,
+				        responsiveClass:true,
+				        responsive:{
+				            0:{
+				                items:1,
+				            },
+				            600:{
+				                items:2,
+				            },
+				            1000:{
+				                items:2,
+				            },
+				            1200:{
+				                items:3,
+				            }
+				        }
+				    });*/
+					
+		
 				}
 				
 			});
